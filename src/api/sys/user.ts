d903abd4f60cc597b6c0439f130656857fd945be
xuanyu <969718197@qq.com>
2021-07-31 20:53:38 +0800

    添加用户权限管理

---
+++ src/api/sys/user.ts
@@ -0,0 +1,41 @@
+import { defHttp } from '/@/utils/http/axios';
+import { LoginParams, LoginResultModel, GetUserInfoModel } from './model/userModel';
+
+import { ErrorMessageMode } from '/#/axios';
+
+enum Api {
+  Login = '/auth/admin/login',
+  Logout = '/logout',
+  GetUserInfo = '/auth/admin/info',
+  GetPermCode = '/getPermCode',
+}
+
+/**
+ * @description: 管理员登录
+ */
+export function loginApi(params: LoginParams, mode: ErrorMessageMode = 'modal') {
+  return defHttp.post<LoginResultModel>(
+    {
+      url: Api.Login,
+      params,
+    },
+    {
+      errorMessageMode: mode,
+    }
+  );
+}
+
+/**
+ * @description: 获取管理员信息
+ */
+export function getUserInfo() {
+  return defHttp.get<GetUserInfoModel>({ url: Api.GetUserInfo });
+}
+
+export function getPermCode() {
+  return defHttp.get<string[]>({ url: Api.GetPermCode });
+}
+
+export function doLogout() {
+  return defHttp.get({ url: Api.Logout });
+}
---
+++ src/enums/httpEnum.ts
@@ -0,0 +1,40 @@
+/*
+ * @Author: xuanyu
+ * @LastEditors: xuanyu
+ * @email: 969718197@qq.com
+ * @github: https://github.com/z-xuanyu
+ * @Date: 2021-07-27 16:38:01
+ * @LastEditTime: 2021-07-27 17:43:15
+ * @Description: Modify here please
+ */
+/**
+ * @description: Request result set
+ */
+export enum ResultEnum {
+  SUCCESS = 1,
+  ERROR = 0,
+  TIMEOUT = 401,
+  TYPE = 'success',
+}
+
+/**
+ * @description: request method
+ */
+export enum RequestEnum {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+}
+
+/**
+ * @description:  contentTyp
+ */
+export enum ContentTypeEnum {
+  // json
+  JSON = 'application/json;charset=UTF-8',
+  // form-data qs
+  FORM_URLENCODED = 'application/x-www-form-urlencoded;charset=UTF-8',
+  // form-data  upload
+  FORM_DATA = 'multipart/form-data;charset=UTF-8',
+}
